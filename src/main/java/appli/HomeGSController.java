f1c2a383793f6e9ecdae76f4d4cd5d450082742e
IbrahimKo95 <ibrahkonate95@gmail.com>
2024-01-17 19:00:07 +0100

    add: Changement de système pour afficher les pages

--- src/main/java/appli/HomeGSController.java
+++ src/main/java/appli/HomeGSController.java
@@ -2,30 +2,34 @@ package appli;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import models.GS;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class HomeGSController implements Initializable {
     @FXML
     private Label UsernameLabel;
-    @FXML
-    private AnchorPane AccueilPane;
 
     @FXML
-    private AnchorPane FournisseursPane;
+    private AnchorPane basePane;
 
     @FXML
     private Button btnAccueil;
 
     @FXML
     private Button btnFournisseurs;
+
+
     private GS utilisateur;
 
     public HomeGSController(GS utilisateur) {
@@ -34,24 +38,29 @@ public class HomeGSController implements Initializable {
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
         UsernameLabel.setText(utilisateur.getPrenom()+" "+utilisateur.getNom());
-    }
-
-    void fermerPane() {
-        AccueilPane.setVisible(false);
-        FournisseursPane.setVisible(false);
-
-    }
-
-    @FXML
-    void changePage1(ActionEvent event) {
-        fermerPane();
-        AccueilPane.setVisible(true);
+        try {
+            Pane pane = FXMLLoader.load(getClass().getResource("/appli/pages/GS/Accueil.fxml"));
+            basePane.getChildren().add(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
-    void changePage2(ActionEvent event) {
-        fermerPane();
-        FournisseursPane.setVisible(true);
+    void changePage(ActionEvent event) throws IOException {
+        basePane.getChildren().setAll();
+        Button button = (Button) event.getSource();
+        if(Objects.equals(button.getText(), "Fournisseurs")) {
+            changePane("fournisseurs");
+        } else if (Objects.equals(button.getText(), "Accueil")) {
+            changePane("Accueil");
+        }
     }
+
+    void changePane(String name) throws IOException {
+        Pane pane = FXMLLoader.load(getClass().getResource("/appli/pages/GS/"+name+".fxml"));
+        basePane.getChildren().add(pane);
+    }
+
 
 }
